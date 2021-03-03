5ec8f851f00dcd2b387e18e1025b1568bc8bb422
ortiwii <48752106+ortiwii@users.noreply.github.com>
2021-03-03 02:30:22 +0100

    Update crypto.py

--- lab1_ws/crypto.py
+++ lab1_ws/crypto.py
@@ -1,9 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+#
+# THIS EXAMPLE HAS BEEN UPDATED TO WORK WITH THE BETA VERSION 12 OF PYTHON-TELEGRAM-BOT.
+# If you're still using version 11.1.0, please see the examples at
+# https://github.com/python-telegram-bot/python-telegram-bot/tree/v11.1.0/examples
+
+"""
+Simple Bot to reply to Telegram messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
 import requests
 import json
-import time
+import logging
 
-# Programa nagusia
-if __name__ == '__main__':
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def pizza(update, context):
     uri = "https://eth.2miners.com/api/accounts/0x3C99c18A9DB8063F2edB586AE24E9d05DD045b2e"
     metodoa = 'GET'
     goiburuak = {'Host': 'eth.2miners.com'}
@@ -20,9 +68,55 @@ if __name__ == '__main__':
     erantzuna = requests.request(metodoa, uri, headers=goiburuak, data=edukia)
     edukia = json.loads(erantzuna.content)
     price = float(edukia["price"])
-    act = money*price
-    print("ETHEREUM PRICE (eur) = " +str(price))
-    print("MONEY (eth) = "+str(money))
-    print("\nMONEY (eur) = "+str(act)+" €")
-    time.sleep(20)
+    act = money * price
+    #print("ETHEREUM PRICE (eur) = " + str(price))
+    #print("MONEY (eth) = " + str(money))
+    #print("\nMONEY (eur) = " + str(act) + " €")
+    update.message.reply_text("ETHEREUM PRICE (eur) = " + str(price))
+    update.message.reply_text("MONEY (eth) = " + str(money))
+    update.message.reply_text("MONEY (eur) = " + str(act) + " €")
 
+def sumar(update,context):
+    try:
+        numero1 = int(context.args[0])
+        numero2 = int(context.args[1])
+
+        suma = numero1 + numero2
+
+        update.message.reply_text("La suma es "+str(suma))
+
+    except (ValueError):
+        update.message.reply_text("por favor utilice dos numeros")
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("1638741516:AAEosfIW_f2qIAIRSvgSMq5Ta-dQQSJ1Cl4", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("sumar", sumar))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, pizza))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
